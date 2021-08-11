5053ef0f0fe6d2bce83a11c328bbd28b7b139e54
Swotwor <swotwor@gmail.com>
2021-08-11 14:29:47 +0300

    Start

---
+++ src/components/PostForm.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const PostForm = () => {
+	return (
+		<div>
+			<form>
+				<input type="text" />
+				<input type="text" />
+				<button></button>
+			</form>
+		</div>
+	)
+}
+
+export default PostForm
